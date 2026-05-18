161adb10cbfd1789e063101084fb33cf6246a229
omar <omarelsayedmuhammad@gmail.com>
2026-05-18 23:58:58 +0300

    update: Optimized Bubble Sort raising the best O time to n instead of n^2

--- algorithms/bubbleSort.ts
+++ algorithms/bubbleSort.ts
@@ -1,13 +1,21 @@
-export function bubbleSort(arr: (string | number)[]): (string | number)[] {
+// Time O(n^2) ~ O(n) | Space O(1)
+type Element = string | number;
+
+export function bubbleSort(arr: Element[]): Element[] {
   for (let o = arr.length - 1; o > 0; --o) {
+    let swap = false;
+
     for (let i = 0; i < o; ++i) {
       // if current bigger than next => swap
-      if (arr[i] > arr[i + 1]) [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swap = true;
+      }
     }
+
+    // if we didn't swap we're done 🥳🥳
+    if (!swap) return arr;
   }
 
   return arr;
 }
-
-console.log(bubbleSort([1, 43, 85, 3, 10, 54]));
-console.log(bubbleSort(["medo", "khillo", "bodii", "abdo"]));
